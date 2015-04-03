6aa78de84c34bafe50b30d748842adf166c157b0
Angel Buzany <angelbuzany@gmail.com>
2015-04-02 21:20:17 -0600

    Update copiright

--- BuzNetSec/Properties/AssemblyInfo.cs
+++ BuzNetSec/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuzNetSec")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright Angel Buzany ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
